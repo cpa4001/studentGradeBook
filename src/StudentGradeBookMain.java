19422b339681c4be468acf739b974a18f0210d79
cpa4001 <cpapostoli4001@eagle.fgcu.edu>
2020-10-24 15:48:27 -0400

    10/24/2020

--- src/StudentGradeBookMain.java
+++ src/StudentGradeBookMain.java
@@ -12,35 +12,46 @@ import java.io.*;
 public class StudentGradeBookMain {
     public static void main(String[] args) throws IOException{
 
-        final String[] CLASS_SELECTION = {"Computational Math", "Graph Theory", "Differential Equations",
+        final String[] AVAILABLE_CLASSES = {"Computational Math", "Graph Theory", "Differential Equations",
                                            "Linear Algebra", "Discrete Math", "Math History", "Statistics"};
         Scanner scan = new Scanner(System.in);
 
         System.out.print("What is your first and last name: ");
         String studentName = scan.nextLine();
+        //Student's name will be split into first name and last name
         String[] studentFullName = studentName.split(" ");
 
+        //Creates a student object using the first two elements in studentFullName
         Student student = new Student(studentFullName[0],studentFullName[1]);
         System.out.print("What class are you entering for: ");
-        String classPick = scan.nextLine();
-        boolean studentInput = true;
-        String studentYesOrNo;
-        while(studentInput) {
-            while (!Arrays.asList(CLASS_SELECTION).contains(classPick)) {
+        String classSelected = scan.nextLine();
+
+        boolean studentGradeInput = true;
+        boolean studentGradeInputCurrentClass = true;
+        String studentYesOrNo = "y";
+        while(studentGradeInput) {
+            while (!Arrays.asList(AVAILABLE_CLASSES).contains(classSelected)) {
+                //while the class selected by the student is available/exists, the console will prompt for correct class
                 System.out.print("Invalid class, please enter a correct class: ");
-                classPick = scan.nextLine();
+                classSelected = scan.nextLine();
+            }
+            while(studentGradeInputCurrentClass){
+                System.out.print("Are you done entering grades for this class(y/n)");
+                studentYesOrNo = scan.nextLine();
+                //Student's choice of y will ex
+                if(studentYesOrNo.toLowerCase().equals("y")){
+                    studentGradeInput = false;
+                } /* else{
+
+                }
+                */
             }
 
-            System.out.print("Are you done entering grades for this class(y/n)");
-            studentYesOrNo = scan.nextLine();
-            if(studentYesOrNo.toLowerCase().equals("y")){
-                studentInput = false;
-            }
             System.out.println("Are you done entering grades? (y/n)");
             studentYesOrNo = scan.nextLine();
 
             if(studentYesOrNo.toLowerCase().equals("y")){
-                studentInput = false;
+                studentGradeInput = false;
             }
         }
     }
