dbf7f64c99125b70944823e3411a3b7548617a87
cpa4001 <cpapostoli4001@eagle.fgcu.edu>
2020-10-24 16:04:19 -0400

    10/24/2020

--- src/Assignment.java
+++ src/Assignment.java
@@ -5,11 +5,17 @@ public class Assignment {
     protected double assignmentWeight;
     LocalDate date = LocalDate.now();
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    /*
+        Assignment Weights
+        Quizzes: 20% (225)
+        Homework: 25% (100)
+        Project: 25% (125)
+        Maximum Points: 700
+     */
 
     public Assignment(){
+        //If no assignment name is given, name will be generated for current date
         this.assignmentName = "Class Assignment" + date.format(formatter);
-        this.assignmentWeight = 0.30;
-        Classroom.assignmentsAndTests.add(assignmentName);
     }
     public Assignment(String assignmentName){
         //We will keep the weight all 25%
